33edef9e93683cf65693d472206d79450d042681
tangxuyao <daulgas.tang@stringstech.co>
2018-03-09 18:38:04 +0800

    temp commit

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"time"
 	"github.com/streadway/amqp"
 	"fmt"
+	"gopkg.in/mgo.v2"
+	"github.com/tangxuyao/mongo"
 )
 
 func failOnError(err error) {
@@ -14,29 +16,38 @@ func failOnError(err error) {
 
 func main() {
 
-	conn, err := amqp.Dial("amqp://guest:guest@127.0.0.1:5672/")
+	conn, err := amqp.Dial("amqp://127.0.0.1:5672/")
 	failOnError(err)
 
 	ch, err := conn.Channel()
 	failOnError(err)
 	defer ch.Close()
 
-	//q, err := ch.QueueDeclare(
-	//	"actors",
-	//	false,
-	//	false,
-	//	false,
-	//	false,
-	//	nil,
-	//)
-	//failOnError(err)
+	q, err := ch.QueueDeclare(
+		"actors",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err)
 
-	//ms, err := mgo.Dial("")
-	//failOnError(err)
+	ms, err := mgo.Dial("")
+	failOnError(err)
 
-	//actorCol := ms.DB("crm").C("actors")
+	colActors := ms.DB("crm").C("actors")
 
 	for {
+		var results []mongo.Charactor
+		colActors.Find(nil).All(&results)
+
+		if len(results) > 0 {
+
+
+		}
+
+
 		fmt.Println("actor_hold_service running..")
 		time.Sleep(time.Second)
 	}
